9185ed0b34f50f98d6cacac804e9e2d36a06e43b
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2022-04-07 06:03:17 +0000

    add handler2

--- server/server.go
+++ server/server.go
@@ -10,12 +10,13 @@ import (
 func main() {
 	fmt.Println("[ Server Started at: http://localhost:8080 ]")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/2", handler2)
 	http.ListenAndServe("localhost:8080", nil)
 }
 
 func handler(res http.ResponseWriter, req *http.Request) {
-	log.Println("---- Start the handler ----")
-	defer log.Println("---- Finish the handler ----")
+	log.Println("---- Start the handler [1] ----")
+	defer log.Println("---- Finish the handler [1] ----")
 
 	// fork the request context
 	ctx := req.Context()
@@ -24,10 +25,31 @@ func handler(res http.ResponseWriter, req *http.Request) {
 	select {
 	// our main code
 	case <-time.After(time.Second * 5):
-		res.Write([]byte("Hello Go!"))
+		res.Write([]byte("Hello Go! [1]"))
 	// if the client abort the loading, so just exit! don't wast the resources for it! 💛
 	case <-ctx.Done():
-		log.Fatalln("---- Server Time Out! ----")
-		http.Error(res, ctx.Err().Error(), http.StatusInternalServerError)
+		fmt.Println("---- Server Time Out! [1] ----")
+		// log.Fatalln("---- Server Time Out! [1] ----")
+		// http.Error(res, ctx.Err().Error(), http.StatusInternalServerError)
+	}
+}
+
+func handler2(res http.ResponseWriter, req *http.Request) {
+	log.Println("---- Start the handler [2] ----")
+	defer log.Println("---- Finish the handler [2] ----")
+
+	// fork the request context
+	ctx := req.Context()
+
+	// a long process :)
+	select {
+	// our main code
+	case <-time.After(time.Second * 4):
+		res.Write([]byte("Hello Go! [2]"))
+	// if the client abort the loading, so just exit! don't wast the resources for it! 💛
+	case <-ctx.Done():
+		fmt.Println("---- Server Time Out! [2] ----")
+		//log.Fatalln("---- Server Time Out! [2] ----")
+		//http.Error(res, ctx.Err().Error(), http.StatusInternalServerError)
 	}
 }
